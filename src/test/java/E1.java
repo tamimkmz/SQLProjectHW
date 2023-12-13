7bd88a160c63e2030be95c88a0eeda67f86376cc
tamimkmz <tamim.kmz@gmail.com>
2023-12-12 19:59:56 -0800

    SQLProjectHW

---
+++ src/test/java/E1.java
@@ -0,0 +1,13 @@
+public class E1 {
+    public static void main(String[] args) {
+        /*
+        jdbc=> api that we are planning to use from java
+        mysql=> name of the database to which we are connecting
+        3.239.253.255 => address of the computer that as our database
+        3306=> port of the application
+        syntaxhrm_mysql => name of the database
+        */
+
+
+        }
+}
